a56bbbe6fd799be5a65fc634477c56e70610aaac
DESKTOP-JHQHHHB\hogi_ghcjssla <mantto2@naver.com>
2017-05-17 12:35:32 +0900

    컴포넌트스캔까지

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,18 +1,40 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-//이 클래스가 웹 애플리케이션에서 요청을 받아들이는 컨트롤러 클래스임을 나타냄
-@RestController
+
+import com.example.domain.Customer;
+import com.example.service.CustomerService;
 //스프링 애플리케이션에 필요했던 다양한 설정등이 자동으로 수행
 @EnableAutoConfiguration
-//JavaConfig를 읽어들이기 위해 @Import로 @Configuration 애너테이션이 붙은 클래스를 지정
-@Import(AppConfig.class)
-public class App {
+//이 클래스 패키지 내부에 있는 모든 클래스를 검색 대상 패키지를 변경하려면 basePackages 속성에 패키지를 지정
+@ComponentScan
+public class App implements CommandLineRunner{
+	@Autowired
+	CustomerService customerService;
+	
+	
 	@RequestMapping("/")
 	String home(){
 		return "Hello world";
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		//데이터 추가
+		customerService.save(new Customer(1,"AAA", "AAA1"));
+		customerService.save(new Customer(2,"BBB", "BBB2"));
+		customerService.save(new Customer(3,"CCC", "CCC3"));
+		
+		//데이터 표시
+		customerService.findAll().forEach(System.out::println);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
 	}
 }
