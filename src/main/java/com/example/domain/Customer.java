2d12b99d954f8a021ae2ad05c70058c9a3e7e2e5
ghcjssla <ghcjssla@gmail.com>
2017-06-21 09:07:29 +0900

    가라!!!버그여

--- src/main/java/com/example/domain/Customer.java
+++ src/main/java/com/example/domain/Customer.java
@@ -12,7 +12,7 @@ import lombok.Data;
 //모든 필드를 인자로 받는 생성자를 만든다.
 @AllArgsConstructor
 @Entity
-@Table(name = "customers")
+@Table(name = "CUSTOMERS")
 public class Customer {
 	@Id
     @GeneratedValue
--- src/test/java/com/example/DemoTestApplicationTests.java
+++ src/test/java/com/example/DemoTestApplicationTests.java
@@ -5,9 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
+@RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class DemoTestApplicationTests {
 
 	@Test
 	public void contextLoads() {
